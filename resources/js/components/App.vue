d4dfba626289dd59d3f9499c02a4dd3aded49aa1
Henry Duvan Bonilla <ing.henry.bonilla@gmail.com>
2024-09-09 22:11:53 -0500

    init

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>¡Hola desde Vue en Laravel!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style scoped>
+/* Estilos específicos del componente */
+</style>
